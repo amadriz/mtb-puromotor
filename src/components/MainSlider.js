993592979e68acde5211560ac289f6072b8ccc5b
Allan Madriz <ahovenga@gmail.com>
2022-04-07 12:27:00 -0600

    working on main slider with video background

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {Autoplay} from 'swiper';
+// swiper is for carousels
+import 'swiper/css/autoplay';
+// powerslap for video background
+import bgVideo from "../assets/videos/bgWebMain.mp4";
+
+
 
 export const MainSlider = () => {
   return (
-    <section className="bg-dark text-white position-relative overflow-hidden">
+    <section className="bg-dark text-white position-relative overflow-hidden mt-100">
     {/* <!--Video--> */}
-    <div className="w-100 h-100 opacity-25 position-absolute end-0 top-0 bg-cover bg-no-repeat bg-center mainSliderBgImg">
-        <div className="jarallax h-100 w-100" data-jarallax-video="mp4:../../src/assets/videos/officeloop.mp4">
-        </div>
-    </div>
+    {/* w-100 h-100 opacity-50 position-absolute end-0 top-0 bg-cover bg-center mainSliderBgImg     bg-no-repeat  " */}
+    <video className="position-absolute opacity-75"
+        autoPlay
+        loop
+        muted   
+      >
+        <source src={bgVideo} type="video/mp4"/>
+      </video>
+        {/* <div className="jarallax h-100 w-100" data-jarallax-video="mp4:../../src/assets/videos/officeloop.mp4">
+        </div> */}
     <div className="container pt-15 pb-12 position-relative z-index-1">
         <div className="row pt-lg-9 pb-5 align-items-center">
             <div className="col-lg-8 mx-auto text-center mb-5 mb-lg-0">
-                <Swiper className="swiper-text w-100 height-3x fs-6 text-uppercase mb-3 text-white overflow-hidden">
+                <Swiper
+                    modules={[Autoplay]}
+                    slidesPerView={1}
+                    autoplay={true}
+                    className="swiper-text w-100 height-3x fs-6 text-uppercase mb-3 text-white overflow-hidden">
                         <SwiperSlide className="justify-content-center">
-                            Design anything
+                            Fotografía deportiva
                         </SwiperSlide>
                         <SwiperSlide className="justify-content-center">
-                            Built for developers
+                            MTB PuroMotor
                         </SwiperSlide>
                         <SwiperSlide className="justify-content-center">
-                            Built for everyone
+                            www.sportivcr.com
                         </SwiperSlide>
                         <SwiperSlide className="justify-content-center">
-                            Mobile friendly
-                        </SwiperSlide>
-                        <SwiperSlide className="justify-content-center">
-                            Best selling theme
+                            Eventos Deportivos
                         </SwiperSlide>
                 </Swiper>
                 <h1 className="display-1 mb-4 mb-lg-5">
-                    We build <span className="text-gradient-light">cutting edge</span> websites
+                    Los mejores en <span className="text-gradient-light">fotografía</span> de ciclismo
                 </h1>
                 <p className="mb-5 mb-lg-7 lead w-lg-60 mx-auto">
-                    Lorem ipsum is placeholder text commonly used in the graphic, print industries.
+                    Somos apacionados del ciclismo
                 </p>
                 <div className="d-flex align-items-center justify-content-center">
-                    <a href="#section" className="btn btn-primary btn-lg me-2 me-lg-3">Get started</a>
+                    <a href="#section" className="btn btn-danger btn-lg me-2 me-lg-3">Ver Galerías</a>
                 </div>
             </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+
+
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      Home
+    </div>
   )
 }
